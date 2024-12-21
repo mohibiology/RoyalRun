3b3d254638b2df410a1f303c0364b527ceef5730
mohibiology <abdulmohib1408@gmail.com>
2024-12-21 12:37:19 +0500

    Set different objects to spawn (Day 32)

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float ObstacleSpawnTime=1f;
-    int obstacleSpawn = 0;
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth;
+
     private void Start() 
     {
         StartCoroutine(SpawnObstacleRoutine());
     }
     IEnumerator SpawnObstacleRoutine()
     {
-        while(obstacleSpawn<5)
+        while(true)
         {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0,obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth,spawnWidth),transform.position.y,transform.position.z);
             yield return new WaitForSeconds(ObstacleSpawnTime);
-            Instantiate(obstaclePrefab,transform.position, Quaternion.identity);
-            obstacleSpawn++;
+            Instantiate(obstaclePrefab,spawnPosition,Random.rotation,obstacleParent);
         }
     }
 }
