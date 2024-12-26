eae29457bcd2e8fd74e08bb2a278e135a506954f
mohibiology <abdulmohib1408@gmail.com>
2024-12-26 22:43:13 +0500

    Stumble animation (Day 36)

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f;
+    const string hitString = "Hit";
+    float collisionCooldownTime = 0f;
+
+    void Update() 
+    {
+        collisionCooldownTime += Time.deltaTime;
+    }
     void OnCollisionEnter(Collision other) 
     {
-        Debug.Log(other.gameObject.name);
+        if(collisionCooldownTime < collisionCooldown)
+            return;
+        animator.SetTrigger(hitString);
+        collisionCooldownTime = 0f;
     }
 }
