6b6b5dd9c8f13c30b8cf9ddeb35e7ed911a5317b
mohibiology <abdulmohib1408@gmail.com>
2025-01-09 22:59:44 +0500

    Music and the end(day 47)

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -9,19 +9,27 @@ public class Rock : MonoBehaviour
     CinemachineImpulseSource cinemachineImpulseSource;
     [SerializeField] float collisionCooldown = 1f;
     float collisionTimer = 0f;
+    Transform player;
     private void Update() {
         collisionTimer+= Time.deltaTime;
     }
     private void Awake() {
         cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        player = playerObject.transform;
     }
     private void OnCollisionEnter(Collision other) {
         if(collisionTimer<collisionCooldown) return;
+        if (!IsInFrontOfPlayer()) return;
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float shakeIntensity = (1f/distance) * shakeModifier;
-        shakeIntensity = Mathf.Min(shakeIntensity, 1f);
+        shakeIntensity = Mathf.Min(shakeIntensity, 0.5f);
         cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
         boulderSmash.Play();
         collisionTimer = 0f;
+    }
+    private bool IsInFrontOfPlayer()
+    {
+        return transform.position.z > player.position.z;
     }
 }
