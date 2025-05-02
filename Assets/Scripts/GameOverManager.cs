138762640c445726db4c1e99a1d4a84adc0e4ad3
mohibiology <abdulmohib1408@gmail.com>
2025-05-02 16:40:56 +0500

    Buttons on gameOver

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,14 +1,29 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] Button retryButton;
+    [SerializeField] Button homeButton;
+    [SerializeField] Button scoreboardButton;
 
     void Start()
     {
         Time.timeScale = 1f; // Reset time scale just in case
         int finalScore = PlayerPrefs.GetInt("FinalScore", 0);
         scoreText.text = finalScore.ToString();
+        retryButton.onClick.AddListener(RetryGame);
+        // homeButton.onClick.AddListener(GoHome);
+    }
+    void RetryGame()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+    void GoHome()
+    {
+        // SceneManager.LoadScene(homeSceneName);
     }
 }
